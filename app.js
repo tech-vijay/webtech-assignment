c5f5522b356c8217aba418d047474f513f422e54
tech-vijay <mauryavijay57737@gmail.com>
2025-04-24 13:28:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function login() {
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+    const errorMsg = document.getElementById('error-message');
+  
+    // const validUser = "vijay";
+    // const validPass = "1234";
+  
+    // if (user === validUser && pass === validPass) {
+    //   window.location.href = "portfolio.html";
+    // } else {
+    //   errorMsg.textContent = "Incorrect username or password!";
+    // }
+
+    window.location.href = "portfolio.html";
+  }
+  
